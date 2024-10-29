dda1fde039acc7a86fb1aa21cbffaccd307e5d2a
solielmaf <miraftsegaye19@gmail.com>
2024-10-29 18:19:12 +0300

    children

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -5,24 +5,34 @@ function App() {
   const [bill, setBill] = useState();
   const [yourTip, setYourTip] = useState("");
   const [yourFriend, setYourFriend] = useState();
+
   function handleBill(e) {
     setBill(e.target.value);
   }
   function handleYourTip(e) {
-    setYourTip(e.target.value);
+    if (e.target.value === "Dissatisified (0%)") setYourTip(0);
+    if (e.target.value === "It was Okay (5%)") setYourTip(5);
+    if (e.target.value === "It was good (10%)") setYourTip(10);
+    if (e.target.value === "Absolutely amazing! (20%)") setYourTip(20);
   }
   function handleFriendTip(e) {
-    setYourFriend(e.target.value);
+    if (e.target.value === "Dissatisified (0%)") setYourFriend(0);
+    if (e.target.value === "It was Okay (5%)") setYourFriend(5);
+    if (e.target.value === "It was good (10%)") setYourFriend(10);
+    if (e.target.value === "Absolutely amazing! (20%)") setYourFriend(20);
   }
   return (
     <div>
       <Bill onchange={handleBill} bill={bill} />
-      <Service
-        onTip={handleYourTip}
-        yourTip={yourTip}
-        onSelect={handleFriendTip}
-        yourFriend={yourFriend}
-      />
+      <Service onTip={handleYourTip} yourTip={yourTip}>
+        How Did you like the Service
+      </Service>
+      <Service onTip={handleFriendTip} yourTip={yourFriend}>
+        How did your friend like the Service
+      </Service>
+      <TipCalculator bill={bill} yourFriend={yourFriend} yourTip={yourTip} />
+      {yourFriend}
+      {yourTip}
     </div>
   );
 }
@@ -34,21 +44,12 @@ function Bill({ onchange, bill }) {
     </div>
   );
 }
-function Service({ onTip, onSelect, yourFriend, yourTip }) {
+function Service({ onTip, onSelect, yourFriend, yourTip, children }) {
   return (
     <>
       <div>
-        How Did you like the Service
+        {children}
         <select value={yourTip} onChange={onTip}>
-          <option>Dissatisified (0%)</option>
-          <option>It was Okay (5%)</option>
-          <option>It was good (10%)</option>
-          <option>Absolutely amazing! (20%)</option>
-        </select>
-      </div>
-      <div>
-        How did your friend like the Service
-        <select value={yourFriend} onChange={onSelect}>
           <option>Dissatisified (0%)</option>
           <option>It was Okay (5%)</option>
           <option>It was good (10%)</option>
@@ -58,5 +59,9 @@ function Service({ onTip, onSelect, yourFriend, yourTip }) {
     </>
   );
 }
-function TipCalculator() {}
+function TipCalculator({ bill, yourFriend, yourTip }) {
+  const [percent, setPercent] = useState();
+
+  const tip = bill * (yourFriend = "Dissatisified (0%)");
+}
 export default App;
