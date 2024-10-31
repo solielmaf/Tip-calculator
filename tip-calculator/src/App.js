aac4c7ae255dab5d4e5eb6bf633e051f98e4b8b2
solielmaf <miraftsegaye19@gmail.com>
2024-10-31 10:51:52 +0300

    last

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -5,41 +5,29 @@ function App() {
   const [bill, setBill] = useState();
   const [yourTip, setYourTip] = useState(0);
   const [yourFriend, setYourFriend] = useState(0);
-  const [tip, setTip] = useState(0);
+  const average = (yourFriend + yourTip) / 2;
+  const tip = (bill * average) / 100;
 
   function handleBill(e) {
     setBill(Number(e.target.value));
   }
-
-  function handleYourTip(e) {
-    const newYourTip = Number(e.target.value);
-    setYourTip(newYourTip);
-    calculateTip(newYourTip, yourFriend);
-  }
-
-  function handleFriendTip(e) {
-    const newYourFriend = Number(e.target.value);
-    setYourFriend(newYourFriend);
-    calculateTip(yourTip, newYourFriend);
-  }
-
-  function calculateTip(yourTip, yourFriend) {
-    const average = (yourFriend + yourTip) / 2;
-
-    setTip((bill * average) / 100);
+  function handleReset() {
+    setBill("");
+    setYourTip(0);
+    setYourFriend(0);
   }
 
   return (
     <div>
       <Bill onchange={handleBill} bill={bill} />
-      <Service onTip={handleYourTip} yourTip={yourTip}>
+      <Service onTip={setYourTip} yourTip={yourTip}>
         How Did you like the Service
       </Service>
-      <Service onTip={handleFriendTip} yourTip={yourFriend}>
+      <Service onTip={setYourFriend} yourTip={yourFriend}>
         How did your friend like the Service
       </Service>
 
-      <TipCalculator tip={tip} bill={bill} />
+      <TipCalculator tip={tip} bill={bill} handleReset={handleReset} />
     </div>
   );
 }
@@ -47,7 +35,12 @@ function Bill({ onchange, bill }) {
   return (
     <div>
       How much was the bill?
-      <input type="number" value={bill} onChange={onchange} />
+      <input
+        type="number"
+        value={bill}
+        onChange={onchange}
+        placeholder="Bill value"
+      />
     </div>
   );
 }
@@ -56,7 +49,7 @@ function Service({ onTip, yourTip, children }) {
     <>
       <div>
         {children}
-        <select value={yourTip} onChange={onTip}>
+        <select value={yourTip} onChange={(e) => onTip(Number(e.target.value))}>
           <option value="0">Dissatisified (0%)</option>
           <option value="5">It was Okay (5%)</option>
           <option value="10">It was good (10%)</option>
@@ -66,12 +59,15 @@ function Service({ onTip, yourTip, children }) {
     </>
   );
 }
-function TipCalculator({ tip, bill }) {
+function TipCalculator({ tip, bill, handleReset }) {
   if (bill > 0)
     return (
-      <h1>
-        you pay {bill} + {tip}
-      </h1>
+      <>
+        <h1>
+          you pay ${bill + tip} (${bill} + ${tip})
+        </h1>
+        <button onClick={handleReset}>Reset</button>
+      </>
     );
 }
 export default App;
